cf16dac7cc35d277f3fec2f0cace23a3974fbc13
zhangguanguan <980253591@qq.com>
2024-08-16 20:49:56 +0800

    first commit

---
+++ src/main/java/com/genetechies/ecust_meeting_room/EcustMeetingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.genetechies.ecust_meeting_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcustMeetingRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcustMeetingRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genetechies/ecust_meeting_room/EcustMeetingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genetechies.ecust_meeting_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcustMeetingRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
