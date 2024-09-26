446869b6f6b98161154013692a3f19bf527bb410
zhangguanguan <980253591@qq.com>
2024-09-26 19:56:38 +0800

    add code

--- src/main/java/com/genetechies/ecust_meeting_room/config/AuthEntryPoint.java
+++ src/main/java/com/genetechies/ecust_meeting_room/config/AuthEntryPoint.java
@@ -1,5 +1,9 @@
 package com.genetechies.ecust_meeting_room.config;
 
+
+import com.genetechies.ecust_meeting_room.pojo.ECUSTResponse;
+import com.genetechies.ecust_meeting_room.pojo.LoginResponse;
+import com.google.gson.Gson;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -14,6 +18,12 @@ import java.io.IOException;
 public class AuthEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,authException.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        ECUSTResponse<String> ecustResponse = new ECUSTResponse<>();
+        ecustResponse.setCode(ECUSTResponse.UNAUTHORIZED);
+        ecustResponse.setMessage("用户名或者密码错误");
+        response.getWriter().write(new Gson().toJson(ecustResponse));
     }
 }
